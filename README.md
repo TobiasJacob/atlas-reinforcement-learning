9dd3d4f4e638ae4514953201bb4795b309b6a844
Tobias Jacob <tobi.jacob@gmx.net>
2021-08-26 17:09:31 -0500

    Added standalone simulator

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# atlas-reinforcement-learning
+# Atlas reinforcement learning
+
+This project aims to run reinforcement learning models on a real Atlas.
+
+- Python version: 3.7.11
+
+## Timeline
+
+- [x] Run the simulated Atlas model in `PyBullet`.
+- [ ] Wrap it into an OpenAI-Gym environment.
+- [ ] Create a second backend vor the OpenAI-Gym that connects to the real Atlas.
+- [ ] Train a very basic machine learning model with `stable_baselines` in the virtual Gym and run it in the real Gym.
+- [ ] See how the model behaves and decide on further steps.
+
+## Setup
+
+[Install Python 3.7](https://linuxize.com/post/how-to-install-python-3-7-on-ubuntu-18-04/). Create a virtual env (or something else).
+
+```console
+which python3.7
+virtualenv -p /usr/bin/python3.7 env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+## Run the code
+
+Run the following command to play around with the Atlas Robot in PyBullet.
+
+
+```console
+python3 atlasrl/simulator/standalone.py
+```
+
+![alt text](docs/AtlasInPyBullet.png)
+
+## State, Actions and Reward
+
+- Observed state is not clear yet.
+- Actions are the pd-controller targets for the 30 joints of the simulated Atlas.
+- Rewards depend on the specific task.
---
+++ atlasrl/simulator/standalone.py
@@ -0,0 +1,51 @@
+"""This is a standalone script to explort how the Atlas model looks like."""
+import pybullet as p
+import pybullet_data
+import time,math
+
+path = "data/atlas/atlas_v4_with_multisense.urdf"
+
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,0)
+atlas = p.loadURDF(path, [-2,3,-0.5])
+for i in range (p.getNumJoints(atlas)):
+	p.setJointMotorControl2(atlas,i,p.POSITION_CONTROL,0)
+	print(p.getJointInfo(atlas,i))
+
+p.loadURDF("plane.urdf",[0,0,-3])
+	
+p.resetDebugVisualizerCamera( cameraDistance=1, cameraYaw=148, cameraPitch=-9, cameraTargetPosition=[0.36,5.3,-0.62])
+
+p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,1)
+
+p.getCameraImage(320,200)#, renderer=p.ER_BULLET_HARDWARE_OPENGL )
+
+print("Num joints", p.getNumJoints(atlas))
+action_selector_ids = []
+for i in range(p.getNumJoints(atlas)):
+    print("Joint info", i, p.getJointInfo(atlas, i))
+    action_selector_id = p.addUserDebugParameter(paramName=str(p.getJointInfo(atlas, i)[1]),
+                                                 rangeMin=-1,
+                                                 rangeMax=1,
+                                                 startValue=0)
+    action_selector_ids.append(action_selector_id)
+
+t=0
+p.setRealTimeSimulation(1)
+while (1):
+    p.setGravity(0,0,-10)
+    time.sleep(0.01)
+    t+=0.01
+    keys = p.getKeyboardEvents()
+    for k in keys:
+        if (keys[k]&p.KEY_WAS_TRIGGERED):
+            if (k == ord('i')):
+                x = 10.*math.sin(t)
+                y = 10.*math.cos(t)
+                p.getCameraImage(320,200,lightDirection=[x,y,10],shadow=1)#, renderer=p.ER_BULLET_HARDWARE_OPENGL )
+
+    for i in range(p.getNumJoints(atlas)):
+        val = p.readUserDebugParameter(action_selector_ids[i])
+        p.setJointMotorControl2(atlas,i,p.POSITION_CONTROL,val)
+
