2c0305dffc3772ab2169f3ebf7676b439edb1b3b
Tobias Jacob <tobi.jacob@gmx.net>
2021-10-08 16:23:16 -0500

    Implemented first half of suggestions

--- atlasrl/motions/MotionState.py
+++ atlasrl/motions/MotionState.py
@@ -1,5 +1,5 @@
 from atlasrl.motions.QuaternionToAtlasEuler import convertQuaternionToAtlasEuler, convertQuaternionToAtlasEulerBack
-from atlasrl.robots.Constants import convertAngleToActionSpace
+from atlasrl.robots.Constants import convertActionSpaceToAngle, convertAngleToActionSpace
 from dataclasses import dataclass
 import dataclasses
 
@@ -91,6 +91,45 @@ class MotionState:
             (1 - alpha) * state1.absoluteTime + alpha * state2.absoluteTime,
         )
     
+    def getAngles(self) -> np.ndarray:
+        angles = np.zeros((30,))
+
+        leftShoulder = convertQuaternionToAtlasEulerBack(self.leftShoulderRotation)
+        rightShoulder = convertQuaternionToAtlasEulerBack(self.rightShoulderRotation)
+        angles[parameterIndex["l_arm_shx"]] = convertActionSpaceToAngle("l_arm_shx", -1)
+        angles[parameterIndex["l_arm_shz"]] = leftShoulder[2]
+        angles[parameterIndex["l_arm_ely"]] = convertActionSpaceToAngle("l_arm_shx", 1)
+        angles[parameterIndex["l_arm_elx"]] = self.leftElbowRotation
+        angles[parameterIndex["r_arm_shx"]] = convertActionSpaceToAngle("l_arm_shx", 1)
+        angles[parameterIndex["r_arm_shz"]] = rightShoulder[2]
+        angles[parameterIndex["r_arm_ely"]] = convertActionSpaceToAngle("l_arm_shx", -1)
+        angles[parameterIndex["r_arm_elx"]] = self.leftElbowRotation
+
+        backEuler = convertQuaternionToAtlasEulerBack(self.chestRotation)
+        angles[parameterIndex["back_bkx"]] = backEuler[0]
+        angles[parameterIndex["back_bky"]] = backEuler[1]
+        angles[parameterIndex["back_bkz"]] = backEuler[2]
+
+        leftHipEuler = convertQuaternionToAtlasEuler(self.leftHipRotation)
+        rightHipEuler = convertQuaternionToAtlasEuler(self.rightHipRotation)
+        angles[parameterIndex["l_leg_hpx"]] = leftHipEuler[0]
+        angles[parameterIndex["l_leg_hpy"]] = leftHipEuler[1]
+        angles[parameterIndex["l_leg_hpz"]] = leftHipEuler[2]
+        angles[parameterIndex["r_leg_hpx"]] = rightHipEuler[0]
+        angles[parameterIndex["r_leg_hpy"]] = rightHipEuler[1]
+        angles[parameterIndex["r_leg_hpz"]] = rightHipEuler[2]
+
+        angles[parameterIndex["l_leg_kny"]] = -self.leftKneeRotation
+        angles[parameterIndex["r_leg_kny"]] = -self.rightKneeRotation
+
+        leftAnkleEuler = convertQuaternionToAtlasEuler(self.leftAnkleRotation)
+        rightAnkleEuler = convertQuaternionToAtlasEuler(self.rightAnkleRotation)
+        angles[parameterIndex["l_leg_akx"]] = leftAnkleEuler[0]
+        angles[parameterIndex["l_leg_aky"]] = leftAnkleEuler[1]
+        angles[parameterIndex["r_leg_akx"]] = rightAnkleEuler[0]
+        angles[parameterIndex["r_leg_aky"]] = rightAnkleEuler[1]
+        return angles
+
 
     def getAction(self) -> np.ndarray:
         action = np.zeros((30,))
--- atlasrl/robots/AtlasBulletEnv.py
+++ atlasrl/robots/AtlasBulletEnv.py
@@ -41,7 +41,7 @@ class AtlasBulletEnv(gym.Env):
 		self._p.setTimeStep(1/(controlFreq * simStepsPerControlStep))
 		self._p.setGravity(0,0,-9.81)
 		self.action_space = gym.spaces.Box(low=-1, high=1, shape=(30,))
-		self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(13 + 2 * 30,))
+		self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(133,))
 		self._p.resetDebugVisualizerCamera(cameraDistance=3, cameraYaw=45, cameraPitch=-30, cameraTargetPosition=self._p.getBasePositionAndOrientation(self.atlas)[0])
 		self._p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
 		self._p.configureDebugVisualizer(p.COV_ENABLE_KEYBOARD_SHORTCUTS, 1)
@@ -56,8 +56,23 @@ class AtlasBulletEnv(gym.Env):
 	def getObservation(self):
 		(pos, orn) = self._p.getBasePositionAndOrientation(self.atlas)
 		posSpeed, ornSpeed = self._p.getBaseVelocity(self.atlas)
-		obs = np.concatenate((pos, p.getEulerFromQuaternion(orn), posSpeed, ornSpeed, [self.time], self.lastDesiredAction, self.lastChosenAction))
-		return obs
+		orn = quaternion.from_float_array((orn[3], *orn[:3]))
+		vecX = quaternion.rotate_vectors(orn, np.array([1, 0, 0]))
+		vecY = quaternion.rotate_vectors(orn, np.array([0, 1, 0]))
+		jointAngles = []
+		jointSpeeds = []
+		for i in range(30):
+			(currentAngle, currentVel, _, _) = self._p.getJointState(self.atlas, i)
+			jointAngles.append(currentAngle)
+			jointSpeeds.append(currentVel)
+		desiredState = self.motionReader.getState(self.time)
+		desiredAngles = desiredState.getAngles()
+		dT = 0.01
+		nextDesiredState = self.motionReader.getState(self.time + dT)
+		nextDesiredAngles = nextDesiredState.getAngles()
+		desiredJointSpeeds = (nextDesiredAngles - desiredAngles) / dT
+		obs = np.concatenate((pos[2:3], vecX, vecY, posSpeed, ornSpeed, jointAngles, jointSpeeds, desiredAngles, desiredJointSpeeds))
+		return obs, desiredAngles, jointAngles, jointSpeeds, desiredJointSpeeds
 
 	def seed(self, seed=None):
 		self.np_random, seed = gym.utils.seeding.np_random(seed)
@@ -65,10 +80,15 @@ class AtlasBulletEnv(gym.Env):
 
 	def reset(self):
 		self._p.restoreState(self.initialState)
-		self.time = 0
-		self.lastDesiredAction = np.zeros(30)
-		self.lastChosenAction = np.zeros(30)
-		return self.getObservation()
+		# Setting atlas to random initial position with noise
+		self.time = np.random.rand() * self.motionReader.frames[-1].absoluteTime
+		motionState = self.motionReader.getState(self.time)
+		action = motionState.getAction() + np.random.normal(30) * 0.1
+		targetPos, targetOrn = motionState.rootPosition, motionState.rootRotation
+		targetOrnAsArray = quaternion.as_float_array(targetOrn)
+		self._p.resetBasePositionAndOrientation(self.atlas, targetPos + np.array([0, 0, 1]), [*targetOrnAsArray[1:4], targetOrnAsArray[0]])
+		self._p.setJointMotorControlArray(self.atlas, np.arange(30), p.POSITION_CONTROL, action)
+		return self.getObservation()[0]
 
 	def render(self, mode = "human", close=False):
 		keys = p.getKeyboardEvents()
@@ -88,57 +108,59 @@ class AtlasBulletEnv(gym.Env):
 		self._p.disconnect()
 
 	def step(self, action):
+		# Get action in residual style
 		desiredState = self.motionReader.getState(self.time)
 		desiredAction = desiredState.getAction()
 		action = desiredAction + action / 5.
 		# action = action / 5.
 
+		# Step simulation
 		self._p.setJointMotorControlArray(self.atlas, np.arange(30), p.POSITION_CONTROL, action)#, forces=[10000] * 30) #, positionGain=0, velocityGain=0)
 		(pos, orn) = self._p.getBasePositionAndOrientation(self.atlas)
 		for _ in range(self.simStepsPerControlStep):
 			self._p.stepSimulation()
 			# sleep(self._p.getPhysicsEngineParameters()["fixedTimeStep"])
-		# Action and action speed difference
-		desiredDifference = desiredAction - self.lastDesiredAction
-		chosenDifference = action - self.lastChosenAction
-		actionDiff = np.square(desiredAction - action).mean()
-		rewardAction = np.exp(-10 * actionDiff) # Too loose
-		actionSpeedDiff = np.square(desiredDifference - chosenDifference).mean()
-		rewardActionSpeed = np.exp(-5 * actionSpeedDiff)
-		self.lastDesiredAction = desiredAction
-		self.lastChosenAction = action
-
-		eulerDif = 2 * np.arccos(quaternion.as_float_array(desiredState.rootRotation.conjugate() * quaternion.from_float_array((orn[3], *orn[:3])))[0])
-		rewardGlobalRotDiff = np.exp(-10 * eulerDif)
-		posDif = np.square(desiredState.rootPosition - pos).mean()
-		rewardRootPosDiff = np.exp(-2 * posDif)  # To strict
+		self._p.resetDebugVisualizerCamera(cameraDistance=3, cameraYaw=self.cameraStates[self.activeI][0], cameraPitch=self.cameraStates[self.activeI][1], cameraTargetPosition=self._p.getBasePositionAndOrientation(self.atlas)[0])
 		self.time += self.timeDelta
+
+		# Observe
+		obs, desiredAngles, jointAngles, jointSpeeds, desiredJointSpeeds = self.getObservation()
+	
+		# Calculate Reward
+		jointDiff = np.square(desiredAngles - jointAngles).mean()
+		rewardJoint = np.exp(-10 * jointDiff)
+		jointSpeedDiff = np.square(jointSpeeds - desiredJointSpeeds).mean()
+		rewardJointSpeed = np.exp(-5 * jointSpeedDiff)
+		rotationDif = 2 * np.arccos(quaternion.as_float_array(desiredState.rootRotation.inverse() * quaternion.from_float_array((orn[3], *orn[:3])))[0] - 1e-5)
+		rewardGlobalRotDiff = np.exp(-10 * rotationDif)
+		posDif = np.square(desiredState.rootPosition - pos).mean()
+		rewardRootPosDiff = np.exp(-2 * posDif)
 		done = self.time > 10
 
+		reward = 0.3 * rewardJoint + 0.1 * rewardJointSpeed + 0.65 * rewardGlobalRotDiff + 0.1 * rewardRootPosDiff
+		if np.isnan(reward):
+			reward = 0
+		
+		# Check for termination because of ground contacts
 		robot_ground_contacts = self._p.getContactPoints(bodyA=self.atlas, bodyB=self.plane)
-
 		for contact in robot_ground_contacts:
 			if contact[3] not in self.footLinks:
 				done = True
 				break
 
-			# rewardDead -= 1
-		reward = 0.3 * rewardAction + 0.1 * rewardActionSpeed + 0.65 * rewardGlobalRotDiff + 0.1 * rewardRootPosDiff
-		if np.isnan(reward):
-			reward = 0
-		reward = np.clip(reward, 0, 2) # Clip reward for unexpected nans
+		# Logging
 		if self.isRender:
-			self.logger.add_scalar("rollout/rewardAction", rewardAction, self.globalStep)
-			self.logger.add_scalar("rollout/rewardActionSpeed", rewardActionSpeed, self.globalStep)
+			self.logger.add_scalar("rollout/rewardJoint", rewardJoint, self.globalStep)
+			self.logger.add_scalar("rollout/rewardJointSpeed", rewardJointSpeed, self.globalStep)
 			self.logger.add_scalar("rollout/rewardGlobalRotDiff", rewardGlobalRotDiff, self.globalStep)
 			self.logger.add_scalar("rollout/rewardRootPosDiff", rewardRootPosDiff, self.globalStep)
 			self.logger.add_scalar("rollout/reward", reward, self.globalStep)
-			self.logger.add_scalar("rollout/eulerDif", eulerDif, self.globalStep)
+			self.logger.add_scalar("rollout/eulerDif", rotationDif, self.globalStep)
 			self.logger.add_scalar("rollout/posDif", posDif, self.globalStep)
-			self.logger.add_scalar("rollout/actionDiff", actionDiff, self.globalStep)
-			self.logger.add_scalar("rollout/actionSpeedDiff", actionSpeedDiff, self.globalStep)
+			self.logger.add_scalar("rollout/jointDiff", jointDiff, self.globalStep)
+			self.logger.add_scalar("rollout/jointSpeedDiff", jointSpeedDiff, self.globalStep)
 			if done:
 				self.logger.add_scalar("rollout/episodeLen", self.time, self.globalStep)
 			self.globalStep += 1
-		self._p.resetDebugVisualizerCamera(cameraDistance=3, cameraYaw=self.cameraStates[self.activeI][0], cameraPitch=self.cameraStates[self.activeI][1], cameraTargetPosition=self._p.getBasePositionAndOrientation(self.atlas)[0])
-		return self.getObservation(), reward, done, {}
+
+		return obs, reward, done, {}
